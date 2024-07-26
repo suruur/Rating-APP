705a67c620dc83e3f146d90cee440dd2f1281560
suruur <surrarweesh@gmail.com>
2024-07-26 02:09:14 -0400

    done rating

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,32 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+        likes: 0,
+        dislikes: 0,
+        handleLike: () => {
+            this.setState((prev) => ({likes: prev.likes + 1}))
+        },
+        handleDisLike: () => {
+            this.setState((prevS) => ({
+                dislikes: prevS.dislikes + 1
+            }))
+        }
+       
+    }
   }
   render() {
     return (
      <>
      <h1>Text Content Rating</h1>
+
+     <button className="rating-button" onClick={this.state.handleLike}>
+        Like ({this.state.likes})
+     </button>
+
+     <button className="rating-button" onClick={this.state.handleDisLike}>
+        DisLike ({this.state.dislikes})
+    </button>
      </>
     );
   }
